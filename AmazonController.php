428b5cc04c37ab2bec2e84d2849decc18f653b1a
Gökhan Demir <gdemir3327@gmail.com>
2019-11-20 15:07:13 +0300

    Update AmazonController.php

--- AmazonController.php
+++ AmazonController.php
@@ -134,32 +134,41 @@ class AmazonController extends V4Controller {
     return $data;
   }
 
-    private static function _query_bests($query_url) {
+  private static function _query_bests($query_url) {
 
     $file = file_get_contents($query_url);
 
-    preg_match_all("'<img alt=\"(.*?)\" src=\"(.*?)\" height=\"200\" width=\"200\">'si", $file, $cards);
+    preg_match_all("'<img alt=\"(.*?)\" src=\"(.*?)\" height=\"200\" width=\"200\">'mi", $file, $cards);
     $_names = $cards[1];
     $_images = $cards[2];
 
-    preg_match_all("'<a class=\"a-link-normal a-text-normal\" href=\"(.*?)\">\s*<span class=\"a-size-medium a-color-base a-text-normal\">\s*(.*?)\s*</span>\s*</a>'si", $file,$cards);
-    $_links = $cards[1];
-    $_names = $cards[2];
+    preg_match_all("'<span class=\"aok-inline-block zg-item\">\s*<a class=\"a-link-normal\" href=\"(.*?)\">\s*<span class=\"zg-text-center-align\">'si", $file, $links);
+    $_links = $links[1];
+
+    foreach ($_links as $i => $value)
+      $_links[$i] = "https://www.amazon.com.tr" . $value;
 
     preg_match_all("'<div class=\"a-row a-size-small\">\s*<span class=\"a-size-small a-color-base\">(.*?)</span>\s*</div>'si", $file, $authors);
     $_authors = $authors[1];
 
-    preg_match_all("'<span class=\"a-size-base a-color-price\">\s*<span class=\"p13n-sc-price\">(.*?)</span>\s*</span>'si", $file, $prices);
-    $_prices = $prices[1];
+    preg_match_all("'<li class=\"zg-item-immersion\" role=\"gridcell\">\s*(.*?)\s*</li>'si", $file, $prices_all);
+    $_prices_all = $prices_all[0];
 
+    $_prices = [];
     $_prices_old = [];
     $_prices_percent = [];
     $_publishers = [];
-    foreach ($_prices_old as $i => $value) {
+    foreach ($_prices_all as $key => $value) {
+      preg_match_all("'<span class=\"a-size-base a-color-price\"><span class=\'p13n-sc-price\' >(.*?)</span></span>'si", $value, $output);
+
+      if (!empty($output[0][0])) {
+        $_prices[] = preg_replace("/[^0-9,.|]/", "", $output[1][0]);
+      } else {
+        $_prices[] = NULL;
+      }
       $_publishers[]  = NULL;
       $_prices_percent[] = NULL;
       $_prices_old[] = NULL;
-      $_prices[$i] = preg_replace("/[^0-9,.|]/", "", $_prices[$i]);
     }
 
     if (isset($_names[0])) {
@@ -187,4 +196,3 @@ class AmazonController extends V4Controller {
   }
 }
 ?>
-
